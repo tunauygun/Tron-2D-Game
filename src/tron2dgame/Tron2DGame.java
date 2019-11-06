40c633f9990b196aa92c2d1b475c2a0f578e8baa
S347391269 <S347391269@LDHS-BYT-96974.ad.ocdsb.ca>
2019-11-06 09:37:20 -0500

    Create Project

---
+++ src/tron2dgame/Tron2DGame.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tron2dgame;
+
+/**
+ *
+ * @author S347391269
+ */
+public class Tron2DGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
